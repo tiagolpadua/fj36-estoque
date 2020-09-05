f709537875869ec852a30d3961c286ddb2d7670d
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-09-05 09:07:34 -0300

    ex2.16-1

--- src/br/com/caelum/estoque/cliente/ClienteRmi.java
+++ src/br/com/caelum/estoque/cliente/ClienteRmi.java
@@ -9,6 +9,15 @@ public class ClienteRmi {
 	public static void main(String[] args) throws Exception {
 		EstoqueRmi estoque = (EstoqueRmi) Naming.lookup("rmi://localhost:1099/estoque");
 		ItemEstoque item = estoque.getItemEstoque("ARQ");
-		System.out.println("Quantidade disponível: " + item.getQuantidade());
+
+		System.out.println("Quantidade disponível (0): " + item.getQuantidade());
+		
+		item.setQuantidade(10);
+
+		System.out.println("Quantidade disponível (1): " + item.getQuantidade());
+		
+		item = estoque.getItemEstoque("ARQ");
+
+		System.out.println("Quantidade disponível (2): " + item.getQuantidade());
 	}
 }
